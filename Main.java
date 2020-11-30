a566b949cb4fd37a98fe0cfd6ac7ff16520573ab
sahilsheth <32227270+sahilsheth@users.noreply.github.com>
2020-11-29 17:54:32 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -11,26 +11,31 @@ public class Main {
     // Store current System.out before assigning a new value
     public static PrintStream console = System.out;
 
-    static {
-        try {
-            o = new PrintStream(new File("A.txt"));
-            System.setOut(o);
+//    static {
+//        try {
+//            o = new PrintStream(new File("A.txt"));
+//            System.setOut(o);
 
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        }
+//    }
 
     // Assign o to output stream
 
     public static void main(String[] args) throws IOException {
+        String fileName = args[0];
         StringBuilder fileContents = new StringBuilder();
 
+
         try {
-            File file = new File("input.txt");
+            File file = new File(fileName);
+            o = new PrintStream(new File(fileName.replace(".txt", "") + "_output.txt"));
+            System.setOut(o);
 //            File file = new File("inputTwo.txt");
 //            File file = new File("inputThree.txt");
             Scanner scanner = new Scanner(file);
+
             while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
                 fileContents.append(line);
@@ -43,14 +48,10 @@ public class Main {
 
         String input = String.valueOf(fileContents);
         input.replaceAll("\\s", "");
+        //remove all the comments
+        input = input.replaceAll("(?s:/\\*.*?\\*/)|//.*", "");
         Lexer lex = new Lexer(input);
         ArrayList<Lexer.Token> tokens = Lexer.lexFunc(input);
         Parser parser = new Parser(tokens, o);
     }
 }
-
-
-
-
-
-
